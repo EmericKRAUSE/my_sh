28fe55a40357464c25f413aa6be92c9e9d8e7e6f
Emeric Krause <ekrause@k2r3p4.42mulhouse.fr>
2024-10-14 17:33:20 +0200

    PUUUUUMBA

--- welcome.sh
+++ welcome.sh
@@ -12,7 +12,7 @@ reset="\e[0m"
 gras="\e[1m"
 souligne="\e[4m"
 
-sleep 0.6
+sleep 0.2
 
 echo -e "\n\n"
 echo -e "
@@ -27,20 +27,20 @@ echo -e "
 \t\t$magenta //         //  \ \   //        //   / /   //   / /     \ \      //
 \t\t$cyan((____     //    \ \ //        ((___( (   ((___( (   //   ) )   ((____$reset\n\n\n\n"
 
-sleep 0.4
+sleep 0.2
 
 echo -e "\t\t$cyan|$reset Username: $gras $USER $bleu|\n"
 
-sleep 0.4
+sleep 0.1
 
 echo -e "\t\t$magenta|$reset Machine: $gras $HOSTNAME $magenta|\n"
 
-sleep 0.4
+sleep 0.1
 
 echo -e "\t\t$rouge|$reset Day: $gras $(date +'%A') $rouge|\n"
 
-sleep 0.4
+sleep 0.1
 
 echo -e "\t\t$jaune|$reset Time: $gras $(date +'%T') $jaune|\n\n\n\n"
 
-sleep 0.4
+sleep 0.1
